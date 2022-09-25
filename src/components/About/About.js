94cc405adcc4eb1ab29b7c63ec430fa994d30b38
Emil Boychev <85123165+EmilBoychev@users.noreply.github.com>
2022-09-26 00:04:05 +0300

    Initialization app and add About component

---
+++ src/components/About/About.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+
+export const About = () => {
+    return (
+        <div className="about slin2">
+            <div className="container">
+                <div className="row d_flex">
+                    <div className="col-md-6">
+                        <div className="titlepage">
+                            <h2>About Us</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                                ad minim veniam, quis nostrud exercitation
+                            </p>
+                            <Link className="read_more" to="#">
+                                {" "}
+                                Read More
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="about_img">
+                            <figure>
+                                <img src="images/about.png" alt="#" />
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,96 @@
+import { NavLink } from "react-router-dom";
+
+
+export const Header = () => {
+    // return (
+    //     <header>
+    //         {/* header inner */}
+    //         <div className="header">
+    //             <div className="container-fluid">
+    //                 <div className="row">
+    //                     <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+    //                         <div className="full">
+    //                             <div className="center-desk">
+    //                                 <div className="logo">
+    //                                     <NavLink to="#">
+    //                                         <img src="/public/images/logo.png" alt="#" />
+    //                                     </NavLink>
+    //                                 </div>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                     <div className="col-xl-7 col-lg-7 col-md-9 col-sm-9">
+    //                         <nav className="navigation navbar navbar-expand-md navbar-dark ">
+    //                             <button
+    //                                 className="navbar-toggler"
+    //                                 type="button"
+    //                                 data-toggle="collapse"
+    //                                 data-target="#navbarsExample04"
+    //                                 aria-controls="navbarsExample04"
+    //                                 aria-expanded="false"
+    //                                 aria-label="Toggle navigation"
+    //                             >
+    //                                 <span className="navbar-toggler-icon" />
+    //                             </button>
+    //                             <div className="collapse navbar-collapse" id="navbarsExample04">
+    //                                 <ul className="navbar-nav mr-auto">
+    //                                     <li className="nav-item ">
+    //                                         <NavLink className="nav-NavLink" to="#">
+    //                                             Home
+    //                                         </NavLink>
+    //                                     </li>
+    //                                     <li className="nav-item active">
+    //                                         <NavLink className="nav-NavLink" to="#">
+    //                                             About
+    //                                         </NavLink>
+    //                                     </li>
+    //                                     <li className="nav-item">
+    //                                         <NavLink className="nav-NavLink" to="#">
+    //                                             What we do
+    //                                         </NavLink>
+    //                                     </li>
+    //                                     <li className="nav-item">
+    //                                         <NavLink className="nav-NavLink" to="#">
+    //                                             Pricing
+    //                                         </NavLink>
+    //                                     </li>
+    //                                     <li className="nav-item">
+    //                                         <NavLink className="nav-NavLink" to="#">
+    //                                             Contact Us
+    //                                         </NavLink>
+    //                                     </li>
+    //                                 </ul>
+    //                             </div>
+    //                         </nav>
+    //                     </div>
+    //                     <div className="col-md-2">
+    //                         <ul className="social_icon">
+    //                             <li>
+    //                                 <NavLink to="#">
+    //                                     <i className="fa fa-facebook" aria-hidden="true" />
+    //                                 </NavLink>
+    //                             </li>
+    //                             <li>
+    //                                 <NavLink to="#">
+    //                                     <i className="fa fa-twitter" aria-hidden="true" />
+    //                                 </NavLink>
+    //                             </li>
+    //                             <li>
+    //                                 <NavLink to="#">
+    //                                     <i className="fa fa-NavLinkedin" aria-hidden="true" />
+    //                                 </NavLink>
+    //                             </li>
+    //                             <li>
+    //                                 <NavLink to="#">
+    //                                     <i className="fa fa-instagram" aria-hidden="true" />
+    //                                 </NavLink>
+    //                             </li>
+    //                         </ul>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </header>
+
+    // )
+}
