ebb74a0ac5c64152be94e1aa488024fb62c8cfd1
Emil Boychev <85123165+EmilBoychev@users.noreply.github.com>
2022-10-04 00:23:57 +0300

    Add Banner slider

---
+++ src/components/Banner/BanerSlider/BanerSlider1.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+
+
+export const BanerSlider1 = ({ props }) => {
+
+    return (
+        <div className="carousel-item active">
+            <div className="container-fluid">
+                <div className="carousel-caption relative">
+                    <div className="row d_flex">
+                        <div className="col-md-6">
+                            {/* <img className="bann_img" src="https://us.123rf.com/450wm/domenicogelermo/domenicogelermo2010/domenicogelermo201000206/156606536-close-up-face-of-a-beautiful-woman-with-multicolor-nails-and-fashion-makeup-.jpg?ver=6" alt="#" /> */}
+                            <img className="bann_img" src="https://www.itakeyou.co.uk/wp-content/uploads/2020/09/dark-nails.jpg" alt="#" />
+                            {/* <img className="bann_img" src="https://www.impressmanicure.com/media/wysiwyg/impress/promotions-1x3/bca-nails-launch/mobile.jpg" alt="#" /> */}
+                            {/* <img className="bann_img" src="https://www.linkpicture.com/q/banner_ing.jpg" alt="#" /> */}
+                        </div>
+                        <div className="col-md-6">
+                            <span>01/03</span>
+                            <h1>Eva nail Salon Creating Beauty</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo</p>
+                            <Link className="get_btn" to="" role="button">Get Appoinment</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,30 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import './Banner.css'
-
+import { BanerSlider } from "./BanerSlider/BanerSlider"
+import { BanerSlider1 } from "./BanerSlider/BanerSlider1"
+import { BanerSlider2 } from "./BanerSlider/BanerSlider2"
 
 export const Banner = () => {
+    const [slide, setSlide] = useState(0);
+
+    const nextButtonHandler = () => {
+        setSlide(state => Number(state) + 1);
+        if (slide >= 2) {
+            setSlide(state => state = 2)
+        }
+
+        console.log(slide);
+    }
+    const prevButtonHandler = () => {
+        setSlide(state => Number(state) - 1);
+        if (slide <= 0) {
+            setSlide(state => state = 0);
+        }
+
+        console.log(slide);
+    }
+
     return (
         <section className="banner_main">
             <div id="myCarousel" className="carousel slide banner1" data-ride="carousel">
@@ -12,67 +34,18 @@ export const Banner = () => {
                     <li data-target="#myCarousel" data-slide-to={2} />
                 </ol>
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <div className="container-fluid">
-                            <div className="carousel-caption relative">
-                                <div className="row d_flex">
-                                    <div className="col-md-6">
-                                        <img className="bann_img" src="https://us.123rf.com/450wm/domenicogelermo/domenicogelermo2010/domenicogelermo201000206/156606536-close-up-face-of-a-beautiful-woman-with-multicolor-nails-and-fashion-makeup-.jpg?ver=6" alt="#" />
-                                        {/* <img className="bann_img" src="https://www.itakeyou.co.uk/wp-content/uploads/2020/09/dark-nails.jpg" alt="#" /> */}
-                                        {/* <img className="bann_img" src="https://www.impressmanicure.com/media/wysiwyg/impress/promotions-1x3/bca-nails-launch/mobile.jpg" alt="#" /> */}
-                                        {/* <img className="bann_img" src="https://www.linkpicture.com/q/banner_ing.jpg" alt="#" /> */}
-                                    </div>
-                                    <div className="col-md-6">
-                                        <span>01/03</span>
-                                        <h1>Eva nail Salon Creating Beauty</h1>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo</p>
-                                        <Link className="get_btn" to="" role="button">Get Appoinment</Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="container-fluid">
-                            <div className="carousel-caption relative">
-                                <div className="row d_flex">
-                                    <div className="col-md-6">
-                                        <img className="bann_img" src="https://www.linkpicture.com/q/banner_ing.jpg" alt="#" />
-                                    </div>
-                                    <div className="col-md-6">
-                                        <span>02/03</span>
-                                        <h1>Eva nail Salon Creating Beauty</h1>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo</p>
-                                        <Link className="get_btn" to="" role="button">Get Appoinment</Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="container-fluid">
-                            <div className="carousel-caption relative">
-                                <div className="row d_flex">
-                                    <div className="col-md-6">
-                                        <img className="bann_img" src="https://www.linkpicture.com/q/banner_ing.jpg" alt="#" />
-                                    </div>
-                                    <div className="col-md-6">
-                                        <span>03/03</span>
-                                        <h1>Eva nail Salon Creating Beauty</h1>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo</p>
-                                        <Link className="get_btn" to="" role="button">Get Appoinment</Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <Link className="carousel-control-prev" to="#myCarousel" role="button" data-slide="prev">
+                    {/* <BanerSlider /> */}
+                    {slide === 0 && <BanerSlider />}
+                    {slide === 1 && <BanerSlider1 />}
+                    {slide === 2 && <BanerSlider2 />}
+
+                    <Link className="carousel-control-prev" to="" onClick={prevButtonHandler} role="button" data-slide="prev">
                         <i className="fa fa-long-arrow-left" aria-hidden="true" />
-                        <span className="sr-only">Previous</span>
+                        <span className="sr-only" >Previous</span>
                     </Link>
-                    <Link className="carousel-control-next" to="#myCarousel" role="button" data-slide="next">
+                    <Link className="carousel-control-next" to="" onClick={nextButtonHandler} role="button" data-slide="next">
                         <i className="fa fa-long-arrow-right" aria-hidden="true" />
-                        <span className="sr-only">Next</span>
+                        <span className="sr-only" >Next</span>
                     </Link>
                 </div>
             </div>
